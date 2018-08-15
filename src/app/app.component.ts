668ca706b2e24f15e33f41597e699e6bf6a288a9
kukicadnan <kukicadnan@gmail.com>
2018-08-14 20:47:25 -0700

    updated UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mypwa';
+  apiUrl = 'https://wtg-apidayssanfrancisco.sandbox.auth0-extend.com/pwa';
+  notTodo: string;
+  notTodos: any;
+
+  constructor(private http: HttpClient) {
+    this.getNotTodos();
+   }
+
+  getNotTodos() {
+    this.http.get(this.apiUrl).subscribe((data) => {
+      this.notTodos = data;
+      console.log(this.notTodos);
+    });
+  }
+
+  addNotTodo(){
+    this.http.post(this.apiUrl, {item: this.notTodo}).subscribe((data) => {
+      this.notTodo = '';
+      this.getNotTodos();
+
+      alert('Success');
+    });
+  }
+
+  cancelNotTodo(){
+    this.notTodo = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }    from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -11,6 +13,8 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
